b593844b0a9728ba1eaf6686c9a285232f835f25
Chris Twaddell <ct9233@gmail.com>
2021-01-13 20:39:42 -0800

    Adding Repos to Card

--- 28 - Github Profiles/script.js
+++ 28 - Github Profiles/script.js
@@ -9,13 +9,24 @@ async function getUser(username) {
         const { data } = await axios(APIURL + username)
 
         createUserCard(data)
-
+        getRepos(username)
     } catch(err) {
         if (err.response.status == 404) {
             createErrorCard('No profile with that username')
         }
     }
         
+}
+
+async function getRepos(username) {
+    try { 
+        const { data } = await axios(APIURL + username + '/repos?sort=created');
+    
+        addReposToCard(data)
+    
+    } catch(err) {
+        createErrorCard('Problem fetching repos')
+    }
 }
 
 function createUserCard(user) {
@@ -32,7 +43,7 @@ function createUserCard(user) {
                     <li>${user.public_repos} <strong>Repos</strong></li>
                 </ul>
 
-                <div class="repos"></div>
+                <div id="repos"></div>
             </div>
         </div>
         `
@@ -48,6 +59,22 @@ function createErrorCard(msg) {
     `
 
     main.innerHTML = cardHTML
+}
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById('repos')
+
+    repos
+        .slice(0, 5)
+        .forEach(repo => {
+        const repoEl = document.createElement('a')
+        repoEl.classList.add('repo')
+        repoEl.href = repo.html_url
+        repoEl.target = '_blank'
+        repoEl.innerText = repo.name
+
+        reposEl.appendChild(repoEl)
+    })
 }
 
 
